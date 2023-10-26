d96364286242105586db39d7c9946f4f1981d412
Ben Watt <77812385+continualWolf@users.noreply.github.com>
2023-10-26 18:21:59 +0100

    Web Socket Project Initialisation
    This is just the first commit for a web socket POC there is still work that needs done in terms of creating a reusable and effective Web Socket class

---
+++ WebSockets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using WebSockets;
+
+Server s = new Server("127.0.0.1", 80);
+
+Thread serverThread = new Thread(s.StartListening);
+serverThread.Start();
+
+// for test and POC sake i create my own client to test with
+Thread clientThread = new Thread(StartClientSide);
+clientThread.Start();
+
+static void StartClientSide()
+{
+    TcpClient clientCreate = new TcpClient();
+    clientCreate.Connect(IPAddress.Parse("127.0.0.1"), 80);
+
+    NetworkStream otherStream = clientCreate.GetStream();
+
+    while (true)
+    {
+        Console.WriteLine("Enter Message: ");
+        string? message = Console.ReadLine();
+        if (message is not null) SendMessage(otherStream, message);
+    }
+}
+
+static void SendMessage(NetworkStream stream, string message)
+{
+    byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(message);
+    stream.Write(bytesToSend, 0, bytesToSend.Length);
+}
---
+++ WebSockets/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebSockets
+{
+    public class Server
+    {
+        private static TcpListener? tcpListener { get; set; }
+
+        public Server(string ip, int portNo)
+        {
+            tcpListener = new TcpListener(IPAddress.Parse(ip), portNo);
+
+            tcpListener.Start();
+            Console.WriteLine("Server has started on {0}:{1}, Waiting for a connection…", ip, portNo);
+        }
+
+        public void StartListening()
+        {
+            if(tcpListener is not null)
+            {
+                while (true)
+                {
+                    // connect client
+                    TcpClient client = tcpListener.AcceptTcpClient();
+
+                    NetworkStream stream = client.GetStream();
+
+                    // enter to an infinite cycle to be able to handle every change in stream
+                    while (true)
+                    {
+                        while (!stream.DataAvailable) ;
+
+                        byte[] bytes = new byte[client.Available];
+                        stream.Read(bytes, 0, bytes.Length);
+                        string receivedMessage = Encoding.UTF8.GetString(bytes);
+
+                        Console.WriteLine("Message received: " + receivedMessage);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
